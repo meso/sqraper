b1f18fcffd5b70326ebd1ad6b2277d74007d4a98
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2012-03-21 20:18:23 +0900

    HTTP Headerで文字コードが判別出来なかった場合に、HTMLを見て判断するように

--- test/sqraper.js
+++ test/sqraper.js
@@ -1,9 +1,10 @@
-var should = require('chai').should();
+var should = require('should');
 var sqraper = require('../lib/sqraper');
 
-describe('Google', function() {
+describe('UTF-8', function() {
   it('www.google.com', function(done) {
     sqraper('http://www.google.com/', function(err, $) {
+      should.not.exist(err);
       $('title').text().trim().should.equal('Google');
       done();
     });
@@ -11,7 +12,40 @@ describe('Google', function() {
 
   it('www.google.co.jp', function(done) {
     sqraper('http://www.google.co.jp/', function(err, $) {
+      should.not.exist(err);
       $('title').text().trim().should.equal('Google');
+      done();
+    });
+  });
+});
+
+describe('EUC-JP', function() {
+  it('FC2ホームページ', function(done) {
+    sqraper('http://web.fc2.com/', function(err, $) {
+      $('title').text().trim().should.equal('FC2ホームページ - 無料ホームページスペース');
+      done();
+    });
+  });
+
+  it('朝日新聞', function(done) {
+    sqraper('http://www.asahi.com/', function(err, $) {
+      $('title').text().trim().should.equal('朝日新聞デジタル：朝日新聞社のニュースサイト');
+      done();
+    });
+  });
+});
+
+describe('Shift_JIS', function() {
+  it('歴代内閣', function(done) {
+    sqraper('http://www.kantei.go.jp/jp/rekidainaikaku.html', function(err, $) {
+      $('title').text().trim().should.equal('歴代内閣・内閣制度 -首相官邸ホームページ-');
+      done();
+    });
+  });
+
+  it('ニコモバ紹介ページ', function(done) {
+    sqraper('http://info.nicovideo.jp/pr_nicom/', function(err, $) {
+      $('title').text().trim().should.equal('ニコニコ動画モバイルのご紹介');
       done();
     });
   });
